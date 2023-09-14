991984e18b4890919b5f43403cac3c5eaa685c3f
Joalor64 <rhgarcon@gmail.com>
2023-09-14 08:41:06 -0400

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -38,12 +38,13 @@ using StringTools;
 
 class Main extends Sprite
 {
-	var gameWidth:Int = 1280; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
-	var gameHeight:Int = 720; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
-	var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
+	public static var gameWidth:Int = 1280; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
+	public static var gameHeight:Int = 720; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
 
-	public static var fpsVar:FPS;
+	public static var fpsVar:FPS; // fps and memory thing
 	public static var toast:ToastCore; // credits go to MAJigsaw77
+
+	var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions (Removed from Flixel 5.0.0).
 
 	public static function main():Void
 	{
